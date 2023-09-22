5ae7883d228c60bcadf9b1f383232b11d99d818e
Felipe Leyva <d.leyvad@uniandes.edu.co>
2023-09-21 19:39:44 -0500

    Base project

--- alarms-web-app/src/app/app.component.ts
+++ alarms-web-app/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'alarms-web-app';
+
+  ngAfterViewInit(): void {
+    // Inicializa los popovers después de que el componente y el DOM estén listos
+  }
 }
