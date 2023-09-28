817d9f5dddfc791a59cc175e022310803a9c2be9
felipe <lf.garciar1@uniandes.edu.co>
2023-09-28 08:12:41 -0500

    add labels list

--- alarms-web-app/src/components/TagList.js
+++ alarms-web-app/src/components/TagList.js
@@ -22,7 +22,36 @@ const TagList = ({ tags, onDelete }) => {
 
   return (
     <Paper elevation={3} style={{ padding: '16px', marginTop: '16px' }}>
-
+      <Typography variant="h5">Lista de Etiquetas</Typography>
+      <List>
+        {tags.map((tag, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={tag} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="edit">
+                <EditIcon />
+              </IconButton>
+              <IconButton edge="end" aria-label="delete" onClick={() => handleDeleteClick(tag)}>
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+      <Modal
+        open={deleteConfirmationModalOpen}
+        onClose={() => setDeleteConfirmationModalOpen(false)}
+        aria-labelledby="delete-confirmation-title"
+        aria-describedby="delete-confirmation-description"
+      >
+        <div className="modal">
+          <Typography variant="h6" id="delete-confirmation-title">
+            ¿Estás seguro de que deseas eliminar esta etiqueta?
+          </Typography>
+          <Button onClick={handleDeleteConfirmation} color="secondary">Eliminar</Button>
+          <Button onClick={() => setDeleteConfirmationModalOpen(false)}>Cancelar</Button>
+        </div>
+      </Modal>
     </Paper>
   );
 };
